7674b97dfe24f4dff8c74da61eb2b8238f98f708
markwage <mark.wage@hotmail.com>
2019-12-09 16:21:20 +0100

    test git

--- learnCode/main.py
+++ learnCode/main.py
@@ -1,3 +1,5 @@
+from kivy import *
+
 list1='item1', 'item2', 'item3', 'item4'
 list2={'item1', 'item2', 'item3', 'item4'}   
 
@@ -6,4 +8,8 @@ print(list2)
 
 print(list1[0], "en volgende item", list1[1:4])
 #test
-#test git
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print('naam is', friends[0])
